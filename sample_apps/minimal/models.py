ee595919dbf78ac7935cd6747dd9721863f0b2da
Mike <mixartemev@gmail.com>
2023-09-19 00:07:22 +0300

    /admin

--- sample_apps/minimal/models.py
+++ sample_apps/minimal/models.py
@@ -1,14 +1,13 @@
 from tortoise import fields
-from tortoise_api_model import Model
-from tortoise_api_model.model import User as BaseUser, TsModel
+from tortoise_api_model.model import User as ApiUser, TsModel
 
 
-class User(BaseUser):
+class User(ApiUser):
     posts: fields.ReverseRelation["Post"]
 
 class Post(TsModel):
     id: int = fields.IntField(pk=True)
     text: str = fields.CharField(4095)
     published: bool = fields.BooleanField()
-    user: User = fields.ForeignKeyField('models.User', related_name='posts')
+    user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField('models.User', related_name='posts')
     _name = 'text'
